cd4d75d00d5678c0c57ba7d63a69983c93f61595
Varunreddy <varunsannapureddy@gmail.com>
2025-01-15 22:35:17 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ import { analyticsRoutes } from "./routes/analytics.route.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
